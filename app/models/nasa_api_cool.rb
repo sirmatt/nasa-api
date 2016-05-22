f02ff533e4cf5e3db2ef8f1d07c4c91e624cf2a1
Matt Batista <mattbgroovy@gmail.com>
2016-05-22 13:59:13 -0700

    added require net/http

--- app/models/nasa_api_cool.rb
+++ app/models/nasa_api_cool.rb
@@ -1,7 +1,9 @@
+require 'net/http'
+
 class NasaApiCool < ActiveRecord::Base
   def self.get_api_info
 
-    uri_address = "https://api.nasa.gov/planetary/apod?api_key=#{ENV["NASA_API_KEY"]}"
+    uri_address = "https://api.nasa.gov/planetary/apod?api_key=#{ENV['NASA_API_KEY']}"
     request_to_nasa_api = Net::HTTP.get(URI(uri_address))
 
     JSON.parse request_to_nasa_api
